e4df90a86af23d6e98048b0b63bf09306ab69e0a
dusa_sai_krishna <dsai9441695421@gmail.com>
2025-06-29 20:17:25 +0530

    Second Commit

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,16 +1,49 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnDestroy, OnInit, signal} from '@angular/core';
 
-import { Place } from '../place.model';
-import { PlacesComponent } from '../places.component';
-import { PlacesContainerComponent } from '../places-container/places-container.component';
+import {Place} from '../place.model';
+import {PlacesComponent} from '../places.component';
+import {PlacesContainerComponent} from '../places-container/places-container.component';
+import {inject} from "@angular/core" ;
+// import {HttpClient} from "@angular/common/http";
+import {Subscription} from "rxjs";
+import {PlacesService} from "../places.service";
 
 @Component({
-  selector: 'app-available-places',
-  standalone: true,
-  templateUrl: './available-places.component.html',
-  styleUrl: './available-places.component.css',
-  imports: [PlacesComponent, PlacesContainerComponent],
+    selector: 'app-available-places',
+    standalone: true,
+    templateUrl: './available-places.component.html',
+    styleUrl: './available-places.component.css',
+    imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
-  places = signal<Place[] | undefined>(undefined);
+export class AvailablePlacesComponent implements OnInit, OnDestroy {
+    places = signal<Place[] | undefined>(undefined);
+    // private httpClient = inject(HttpClient);
+    private placeService = inject(PlacesService);
+    isFetching = signal(true);
+    private subscription?: Subscription;
+
+    ngOnInit() {
+        this.subscription = this.placeService.loadAvailablePlaces()
+            .subscribe({
+                next: (resData) => this.places.set(resData.places),
+                complete: () => this.isFetching.set(false)
+            });
+    }
+
+    ngOnDestroy() {
+        this.subscription?.unsubscribe();
+    }
+
+    onSelectPlace(selectedPlace: Place) {
+        this.placeService.addPlaceToUserPlaces(selectedPlace).subscribe({
+            complete: () => {
+                console.log(`Selected Place: ${selectedPlace.id}`)
+            },
+            error: () => {
+                console.error(`Selected Place: ${selectedPlace.id}`)
+            }
+        })
+    }
+
+
 }
--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit, signal} from '@angular/core';
 
-import { PlacesContainerComponent } from '../places-container/places-container.component';
-import { PlacesComponent } from '../places.component';
+import {PlacesContainerComponent} from '../places-container/places-container.component';
+import {PlacesComponent} from '../places.component';
+import {Place} from "../place.model";
+import {Subscription} from "rxjs";
+import {PlacesService} from "../places.service";
 
 @Component({
-  selector: 'app-user-places',
-  standalone: true,
-  templateUrl: './user-places.component.html',
-  styleUrl: './user-places.component.css',
-  imports: [PlacesContainerComponent, PlacesComponent],
+    selector: 'app-user-places',
+    standalone: true,
+    templateUrl: './user-places.component.html',
+    styleUrl: './user-places.component.css',
+    imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit, OnDestroy {
+    places = signal<Place[] | undefined>(undefined);
+
+    private placeService  = inject(PlacesService);
+    private subscription: Subscription | undefined = undefined;
+    error = signal("");
+    isFetching = signal(true);
+
+
+    ngOnInit() {
+        this.subscription = this.placeService.loadUserPlaces().subscribe({
+            next: resData => this.places.set(resData.places),
+            error: error => {
+                console.error(error.message);
+                this.error.set("Something went wrong while fetching favourite places");
+            },
+            complete: () => this.isFetching.set(false)
+        })
+    }
+
+    ngOnDestroy() {
+        this.subscription?.unsubscribe();
+    }
+
 }
